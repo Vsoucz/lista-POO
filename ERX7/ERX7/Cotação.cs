6b07ab8da8fce33d244aae1a990b636a083db059
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-09-29 20:24:27 -0300

    Add files via upload

---
+++ ERX7/ERX7/Cotação.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERX7
+{
+    internal class Cotação
+    {
+        private int cotacaoDoDolar;
+        private int quantidadeDeDolares;
+        private int valorEmReais;
+
+        public void SetCotacaoDoDolar(int cotacao)
+        {
+            this.cotacaoDoDolar = cotacao;
+        }
+
+        public void SetQuantidadeDeDolares(int quantidade)
+        {
+            this.quantidadeDeDolares = quantidade;
+        }
+
+        public void CalcularValorEmReais()
+        {
+            this.valorEmReais = this.cotacaoDoDolar * this.quantidadeDeDolares;
+        }
+
+        public int GetValorEmReais()
+        {
+            return this.valorEmReais;
+        }
+    }
+}
---
+++ ERX7/ERX7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERX7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cotação cotacao = new Cotação();
+
+            Console.WriteLine("Qual é a cotação do dólar? (arredonde para número inteiro):");
+            int cotacaoDoDolar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Quantos dólares você tem? :");
+            int quantidadeDeDolares = Convert.ToInt32(Console.ReadLine());
+
+            cotacao.SetCotacaoDoDolar(cotacaoDoDolar);
+            cotacao.SetQuantidadeDeDolares(quantidadeDeDolares);
+
+            cotacao.CalcularValorEmReais();
+
+            Console.WriteLine("O valor em reais é: R$ " + cotacao.GetValorEmReais());
+        }
+    }
+}
