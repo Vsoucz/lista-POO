ffc3c497c5d008b77c5fbc5f4565b30e24eb4283
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-09-29 20:06:56 -0300

    Add files via upload

---
+++ erx4/erx4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("coloque a base do seu triangulo");
+            int b = int.Parse(Console.ReadLine());
+            triangulo.setBase(b);
+
+            Console.WriteLine("Agora coloque a altura do seu triangulo");
+            int h = int.Parse(Console.ReadLine());
+
+            triangulo.setAltura(h);
+            triangulo.Calcularea();
+
+            Console.WriteLine("A area do seu triangulo é:" + triangulo.getArea());
+            Console.Write("...");
+
+
+        }
+    }
+}
---
+++ erx4/erx4/Triangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erx4
+{
+    internal class Triangulo
+    {
+        private int b;
+        private int h;
+        private int area;
+
+        public void setAltura(int h)
+        {
+            this.h = h;
+        }
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+        public int getBase()
+        {
+            return this.b;
+        }
+        public int getAltura()
+        {
+            return this.h;
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+        public void Calcularea()
+        {
+            this.area = (b * h) / 2; ;
+        }
+
+    }
+}
