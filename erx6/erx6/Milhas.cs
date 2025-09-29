631502fb0c7ec0ee87a25f5c4172c808ab62ad73
Vsoucz <168143673+Vsoucz@users.noreply.github.com>
2025-09-29 20:16:41 -0300

    Add files via upload

---
+++ erx6/erx6/Milhas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erx6
+{
+    internal class Milhas
+    {
+        private int m;
+        private double km;
+
+        public void SetMilhas(int m)
+        {
+            this.m = m;
+        }
+        public int GetMilhas()
+        {
+            return this.m;
+        }
+        public double GetQuilometro()
+        {
+            return this.km;
+        }
+
+        public void Calcular()
+        {
+            this.km = (this.m * 1852) / 1000.00;
+        }
+    }
+}
---
+++ erx6/erx6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erx6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milhas milhas = new Milhas();
+
+            Console.WriteLine("CONVERSOR DE MILHAS EM QUILÔMETROS");
+            Console.WriteLine("Coloque as milhas que você quer converter:");
+
+            int quantidadeMilhas = int.Parse(Console.ReadLine());
+            milhas.SetMilhas(quantidadeMilhas);
+            milhas.Calcular();
+            Console.WriteLine("CONVERTENDO em kms:" + milhas.GetQuilometro());
+            
+        }
+    }
+}
